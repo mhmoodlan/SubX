976671ddc896f053135fc43260278ae638f6a852
Mahmoud Aslan <mahmoudaslan@outlook.com>
2017-07-20 10:45:17 +0300

    Changed the L&F to the cross-platform one.

--- src/subtitlesystemviews/ServerViewer.java
+++ src/subtitlesystemviews/ServerViewer.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package subtitlesystemviews;
 
 import java.io.BufferedReader;
@@ -14,6 +8,7 @@ import java.io.IOException;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 import subtitlesystemcontrollers.ClientController;
 import subtitlesystemcontrollers.ServerController;
 
@@ -507,12 +502,8 @@ public class ServerViewer extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
+            UIManager.setLookAndFeel(
+            UIManager.getCrossPlatformLookAndFeelClassName());
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(ServerViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
